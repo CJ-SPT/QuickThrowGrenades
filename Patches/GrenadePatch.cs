e77c442c09b3fb33feccc9062ea927aed64ee33e
Cj <161484149+CJ-SPT@users.noreply.github.com>
2024-04-02 23:03:52 -0400

    Add project files.

---
+++ Patches/GrenadePatch.cs
@@ -0,0 +1,47 @@
+﻿using Aki.Reflection.Patching;
+using EFT;
+using System.Linq;
+using System.Reflection;
+
+namespace QuickThrowGrenades.Patches
+{
+    internal class GrenadePatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(Player).GetMethods().First(m =>
+                m.Name == "SetInHands" && m.GetParameters()[0].Name == "throwWeap");
+        }
+
+        [PatchPrefix]
+        private static bool Prefix(Player __instance, GrenadeClass throwWeap)
+        {
+
+#if DEBUG
+            ConsoleScreen.Log($"Is Keybind enabled: {Plugin.EnableKeybind.Value}");
+            ConsoleScreen.Log($"Is Keybind down: {Plugin.Keybind.Value.IsDown()}");
+#endif
+
+            // Dont use keyboard shortcut
+            if (Plugin.MainPlayer != null && Plugin.Enable.Value 
+                && !Plugin.EnableKeybind.Value)
+            {
+                __instance.SetInHandsForQuickUse(throwWeap, null);
+            }
+
+            // Use keyboard shortcut
+            if (Plugin.MainPlayer != null && Plugin.Enable.Value
+                && Plugin.EnableKeybind.Value && Plugin.Keybind.Value.IsDown())
+            {
+                __instance.SetInHandsForQuickUse(throwWeap, null);
+            }
+
+            if (Plugin.EnableKeybind.Value && !Plugin.Keybind.Value.IsDown())
+            {
+                return true;
+            }
+
+            return !Plugin.Enable.Value;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,60 @@
+﻿using EFT;
+using System;
+using BepInEx;
+using UnityEngine;
+using BepInEx.Configuration;
+using DrakiaXYZ.VersionChecker;
+using QuickThrowGrenades.Patches;
+using Comfort.Common;
+using static DrakiaXYZ.VersionChecker.VersionChecker;
+
+namespace QuickThrowGrenades
+{
+    [BepInPlugin("com.dirtbikercj.QuickThrowGrenades", "Quick Throw Grenades", "1.0.2")]
+    internal class Plugin : BaseUnityPlugin
+    {
+        public const int TarkovVersion = 29197;
+
+        internal static ConfigEntry<bool> Enable;
+        internal static ConfigEntry<bool> EnableKeybind;
+        internal static ConfigEntry<KeyboardShortcut> Keybind;
+
+        internal static Player MainPlayer = null;
+
+        private void Awake()
+        {
+            if (!CheckEftVersion(Logger, Info, Config))
+            {
+                throw new Exception("Invalid EFT Version");
+            }
+
+            Enable = Config.Bind(
+               "General",
+               "Quick Throw Grenades",
+               false,
+               new ConfigDescription("Enable plugin", null, new ConfigurationManagerAttributes { Order = 3 }));
+
+            EnableKeybind = Config.Bind(
+               "General",
+               "Enable keybind",
+               false,
+               new ConfigDescription("Use A Keybind so you can still throw the old way.", null, new ConfigurationManagerAttributes { Order = 2 }));
+
+            Keybind = Config.Bind(
+               "General",
+               "Quick throw keybind",
+               new KeyboardShortcut(KeyCode.G, KeyCode.LeftShift),
+               new ConfigDescription("Keybind to quick throw grenades - One of these keybinds must be same as BSG's grenade keybind", null, new ConfigurationManagerAttributes { Order = 1 }));
+
+            new GrenadePatch().Enable();
+        }
+
+        private void Update()
+        {
+            if (Singleton<GameWorld>.Instantiated && MainPlayer == null)
+            {
+                MainPlayer = Singleton<GameWorld>.Instance.MainPlayer;
+            }
+        }
+    }
+}
